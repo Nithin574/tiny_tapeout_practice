5f91d56527900530386099d031c54c6a3358f602
Nithin574 <nithins038@gmail.com>
2025-03-07 08:53:57 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,7 +17,7 @@ module tt_um_Nithin574 (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-    reg [6:0] uo_out_temp;
+    reg [7:0] uo_out_temp;
     reg clk_25Mhz;
     
     always@(posedge clk, negedge rst_n) begin
@@ -29,13 +29,13 @@ module tt_um_Nithin574 (
             clk_25Mhz <= clk_25Mhz + 1'b1;
             end
     end
-    assign uo_out[6:0] = uo_out_temp;
+    assign uo_out[7:0] = uo_out_temp;
     always@(posedge clk_25Mhz, negedge rst_n)begin
         if(!rst_n)begin
               uo_out_temp <= 8'd0;
             //clk_25Mhz <= 1'b0;
             end else begin
-            uo_out_temp  <= ui_in[5:0] + uio_in[5:0];  // Example: ou_out is the sum of ui_in and uio_in
+                uo_out_temp  <= ui_in[6:0] + uio_in[6:0];  // Example: ou_out is the sum of ui_in and uio_in
             //clk_25Mhz <= clk_25Mhz + 1'b1;
             end
     end
@@ -46,7 +46,7 @@ module tt_um_Nithin574 (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-    wire _unused = &{ena,ui_in[7:6],uio_in[7:6], 1'b0};
+    wire _unused = &{ena,ui_in[7],uio_in[7], 1'b0};
 /*
 
      // All output pins must be assigned. If not used, assign to 0.
